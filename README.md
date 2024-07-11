1324f19ec8e1beae4623985894190dce807cdf25
nkemdilim <nkemdilim.chime@dci-student.org>
2024-07-11 23:59:06 +0200

    Fundamentals of Python

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+ 
+
 # fundamental-python
+## Yesterday - 2024-07-10
+* Algorithms
+* Tooling (VS code)
+* Virtual environment (pyenv)
+* Python Hello World
+* Exercise
+
+
+## Today - 2024-07-11
+
+* Python module
+* print
+* Variables and Data types
+* Input
+* Operators
+* math fucntions
+* one file python program
+* Exercise
---
+++ introduction.py
@@ -0,0 +1,140 @@
+
+
+print('Hello Python', 'Welcome to Python Module', 2024, sep=' ', end='\n')
+
+name = 'Roger Federer'
+
+age = 34
+Age = 42
+print(name) 
+print(age)
+
+
+'''This is a Comment. 
+It is used for a multiple line comment
+Comment continues
+Theses are called docstrings'''
+
+# carmel case naming (variable)
+personAge = 54
+
+# pascal case naming (variable)
+Personage = 56
+
+# snake case naming
+# person_age_height = '28 and 172 cm'
+# print(Age)
+# print(person_age_height)
+# x = 'green'
+# y = 'blue'
+
+# x,y,z = 'green', 'blue', 'yellow'
+# print(y)
+# x = y = z = 'magaeta'
+# print(z)
+# print(x)
+
+color_1 = 'green'
+color_2 = 'purple'
+color_1, color_2 = color_2, color_1
+
+print(color_2)
+
+# Data types in Python
+
+'''
+string, int, float, bool (boolean), complex, None
+'''
+first_name = 'Thomas'
+age = 22
+weight = 65.7
+is_student = True
+complex_num = 5+7j # mixed and imaginary 
+temperature = None
+
+# print(type(first_name))
+# print(type(age))
+# print(type(weight))
+# print(type(is_student))
+# print(type(complex_num)) # mixed and imaginary 
+# print(type(temperature))
+
+
+# isinstance(age, str)
+# print(isinstance(age, str))
+# print(isinstance(complex_num, complex))
+
+
+# user input
+
+# sport = input('What is your favorite sport?  ')
+# print('I see, your favorite sport is  ',sport)
+
+# print(type(sport))
+
+number = input('Enter your number  ')
+
+#print(number)
+
+#real_number = int(number)
+
+#print(type(number))
+
+'''addtional('), subtraction(-), multplication(*), moduls(%), float division, division(/)'''
+
+num1 = 120
+num2 = 80
+result = num1 + num2
+result = num1 / num2
+
+# complex_number = real_part + complex_part
+um1 = 4+7j
+num2 = 3+2j
+
+result = num1 - num2 # result = 1+5j
+
+um1 = 10
+num2 = 2
+#result = num1 % num2 # modulus gives the remainder of a division
+print(result)
+
+# float division takes the resultant value
+
+maximum_num(44, 35,100, 27, 500, 3,77, 222)
+print(maximum_num)
+
+minimum_num(44, -35, 100, 27, 500, 3, -77, 222)
+
+print(maximum_num)
+
+#absolute number of a number is the positive part of that number
+#number = -2550
+
+abs_num = abs(-78-2)
+print(abs_num) # -80
+
+abs_num = abs(4+3j) # 5 too complicated computation
+
+number = round(12.4567889) # answer is 12
+number = round(12.8567889, 2) # answer is 12.86
+
+pow(4, 2) # 4°2 = 16
+
+pow(3,3) # 3°3 = 27
+
+pow(3, 7) # 3°7 =
+
+import math
+
+number = math.pi # 3.14
+
+print(number) # 
+ 
+number = math.sqrt(49) # 7
+
+number = math.cell(8.4375) # the cell function takes the number it operates and takes the next integer number = 9
+
+number = math.floor(8.4375) # the cell function takes the number it operates and takes the lower integer number = 8
+
+
+
